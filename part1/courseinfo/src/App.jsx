f12f6f78e2b9ff1a1af4b3b34105b525486d0fad
fvaldez29 <fv.2900@hotmail.com>
2024-12-16 22:19:50 -0400

    courseinfo already done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,17 +1,31 @@
-import { Content } from "./components/Content"
-import { Header } from "./components/Header"
-import { Total } from "./components/Total"
-
 const App = () => {
-const course = 'Half Stack application development'
+
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total />
+      <h2>{course}</h2>
+     <p> {parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
+      <p>Total of exercises: {parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)}</p>
     </div>
   )
 }
 export default App
+ 
--- part1/courseinfo/src/components/Header.jsx
+++ part1/courseinfo/src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 // eslint-disable-next-line react/prop-types
 export const Header = ({ course }) => {
-
+    console.log(course)
     return (
         <div>{course}</div>
     )

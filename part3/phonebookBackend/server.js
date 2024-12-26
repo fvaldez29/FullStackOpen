116b531af521d23ecc653426b9b06977e882d03e
fvaldez29 <fv.2900@hotmail.com>
2024-12-26 11:10:01 -0400

    cors header included

--- part3/phonebookBackend/server.js
+++ part3/phonebookBackend/server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
+import 'dotenv/config'
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const notes = [
     {

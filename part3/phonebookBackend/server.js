eb72fa2f267dfc600716db7b1f8f5cc9a118d414
fvaldez29 <fv.2900@hotmail.com>
2025-01-28 10:58:06 -0400

    fyi

--- part3/phonebookBackend/server.js
+++ part3/phonebookBackend/server.js
@@ -87,7 +87,7 @@ app.use(unkwonEndpoint)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 1234
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
